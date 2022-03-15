976932bcb0bbce704f986a9703fcce74d02feb74
Aleksander Kus <01151536@pw.edu.pl>
2022-03-15 17:42:33 +0100

    change to latest kernel

--- system/hardware-configuration.nix
+++ system/hardware-configuration.nix
@@ -12,7 +12,7 @@
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-amd" ];
   boot.extraModulePackages = [ ];
-
+  boot.kernelPackages = pkgs.linuxPackages_latest;
   fileSystems."/" =
     { device = "/dev/disk/by-label/NIXROOT";
       fsType = "ext4";
